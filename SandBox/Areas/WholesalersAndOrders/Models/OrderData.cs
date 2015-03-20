f9b62c33de211d95247b53dbb06f70a41cd01fab
AlexKosh <mailforolin@gmail.com>
2015-03-20 16:16:50 +0200

    Added OrderData.cs file

---
+++ SandBox/Areas/WholesalersAndOrders/Models/OrderData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SandBox.Areas.WholesalersAndOrders.Models
+{
+    public class OrderData
+    {
+        [Key]        
+        public int OrderID { get; set; }
+        public int WholesalerID { get; set; } 
+        public bool Delivery { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime DeliveryDateTime { get; set; }
+        public string Description { get; set; }        
+    }
+}
