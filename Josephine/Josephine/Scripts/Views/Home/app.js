0eb013974a45da9fbf1bc2c746d8cd467e96d155
AlexKosh <mailforolin@gmail.com>
2015-10-13 11:39:20 +0300

    Add StoreNavController

--- Josephine/Josephine/Scripts/Views/Home/app.js
+++ Josephine/Josephine/Scripts/Views/Home/app.js
@@ -263,7 +263,8 @@ app.controller('HomeController', function (dataService, $scope, $modal) {
     }
     //-------------------------------------------------------
     //=========== ------- initials end ------ ===============
-    //=======================================================    
+    //======================================================= 
+
     $scope.setColorOfReadinessForOrder = function (ord) {
         if (ord.isPacked == 'не упакован') {
             return 'panel-danger';
@@ -867,6 +868,36 @@ app.controller('DatepickerDemoController', function ($scope, dataService) {
         $scope.selectedMinDate = $scope.selectedMaxDate = new Date();
         $scope.getSalesData();
     }
+});
+app.controller('StoreNavController', function ($scope) {
+    $scope.numberForActiveClass = 0;
+
+    $scope.storeOnClick = function () {
+        $scope.$parent.leftTableView = $scope.$parent.storeData;
+        $scope.$parent.isSelected = 'st';
+        $scope.numberForActiveClass = 1;
+    };
+    $scope.whOnClick = function () {
+        $scope.$parent.leftTableView = $scope.$parent.warehouseData;
+        $scope.$parent.isSelected = 'wh';
+        $scope.numberForActiveClass = 2;
+    };
+    $scope.ordOnClick = function () {
+        $scope.$parent.leftTableView = {};
+        $scope.$parent.isSelected = 'ord';
+        $scope.numberForActiveClass = 3;
+    };
+    $scope.custOnClick = function () {
+        $scope.$parent.leftTableView = {};
+        $scope.$parent.isSelected = 'cust';
+        $scope.numberForActiveClass = 4;
+    };
+    $scope.salesOnClick = function () {
+        $scope.$parent.leftTableView = {};
+        $scope.$parent.isSelected = 'sales';
+        $scope.numberForActiveClass = 5;
+    }
+
 });
 
 app.factory('dataService', function ($http) {
